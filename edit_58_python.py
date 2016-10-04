821cf8b88fb1485706a19d954e62634fd64fd74c
Jacie3 <jaciejermier@gmail.com>
2016-10-04 13:38:44 -0700

    copy and move modified .txt files (last 24 hours)

---
+++ edit_58_python.py
@@ -0,0 +1,21 @@
+import os
+import time
+import datetime
+import shutil
+
+now = datetime.datetime.now()
+ago = now - datetime.timedelta(hours = 24)
+
+spath = 'C:\Users\Student\Desktop\Folder A'
+dest = 'C:\Users\Student\Desktop\Folder B'
+
+for filename in os.listdir(spath):
+    path = os.path.join(spath, filename)
+    statinfo = os.stat(path)
+    mtime = datetime.datetime.fromtimestamp(statinfo.st_mtime)
+    if mtime > ago and filename.endswith('.txt'):
+        src = os.path.join(spath, filename)
+        dst = os.path.join(filename, dest)
+        shutil.copy(src, dst)
+        print ("{} This file has been modifed within the last 24 hours, and has been copied to {} ".format(filename, dst))
+    
